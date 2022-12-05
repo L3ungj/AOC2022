47a9c8d969771d5a26de47f11bc4f988f1c6c604
L3ungj <justin.leungj@gmail.com>
2022-12-05 19:56:34 +0000

    remove exe and pdb files

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if len(sys.argv) < 2:
 s = sys.argv[1]
 proc = subprocess.run([
     'rustc', f'./src/{s}.rs',
-    '-o', f'./src/{s}.exe'
+    '-o', f'./target/debug/{s}.exe'
 ])
 if proc.returncode != 0:
     exit(0)
@@ -18,5 +18,5 @@ input_string = fi.read() + '\n@#-3aV[./'
 
 fo = open('fo.txt', 'w')
 proc = subprocess.run([
-    f'./src/{s}.exe',
+    f'./target/debug/{s}.exe',
 ],input=input_string.encode('utf-8'), stdout=fo)
