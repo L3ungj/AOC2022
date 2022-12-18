14755c26cbde5204ff0bb003d052eb1b5d4796e0
L3ungj <justin.leungj@gmail.com>
2022-12-18 16:35:23 +0800

    day 16

--- src/16b.rs
+++ src/16b.rs
@@ -12,7 +12,7 @@ fn rdln(s: &mut String) -> bool {
     return false;
 }
 
-fn get_valve(s : &String, mp_valve : &mut HashMap<String, usize>, _cnt: &mut usize) -> usize{
+fn get_valve(s: &String, mp_valve: &mut HashMap<String, usize>, _cnt: &mut usize) -> usize {
     if mp_valve.contains_key(s) {
         return mp_valve[s];
     }
@@ -21,7 +21,7 @@ fn get_valve(s : &String, mp_valve : &mut HashMap<String, usize>, _cnt: &mut usi
     return mp_valve[s];
 }
 
-fn dfs(u: usize,step: ll, vis: &Vec<bool>, dist: &Vec<Vec<ll>>, fr: &Vec<ll>) -> ll{
+fn dfs(u: usize, step: ll, vis: &Vec<bool>, dist: &Vec<Vec<ll>>, fr: &Vec<ll>) -> ll {
     let n = dist.len();
     let mut ma = 0;
     // println!("dfsing {}", u);
@@ -50,25 +50,25 @@ fn dfs(u: usize,step: ll, vis: &Vec<bool>, dist: &Vec<Vec<ll>>, fr: &Vec<ll>) ->
 }
 
 fn main() {
-    let mut mp_valve : HashMap<String, usize> = HashMap::new();
+    let mut mp_valve: HashMap<String, usize> = HashMap::new();
     let mut _cnt = 0;
-    let mut G:Vec<Vec<usize>> = vec![Vec::new();100];
-    let mut fr: Vec<ll> = vec![0;100];
+    let mut G: Vec<Vec<usize>> = vec![Vec::new(); 100];
+    let mut fr: Vec<ll> = vec![0; 100];
     'input_loop: loop {
         let mut s = String::new();
         if rdln(&mut s) {
             break 'input_loop;
         }
-        let tokens : Vec<String> = s.split(';').map(String::from).collect();
-        let valve_pos = s.find("Valve ").unwrap() + 6; 
-        let valve : String = s[valve_pos..valve_pos + 2].to_string();
+        let tokens: Vec<String> = s.split(';').map(String::from).collect();
+        let valve_pos = s.find("Valve ").unwrap() + 6;
+        let valve: String = s[valve_pos..valve_pos + 2].to_string();
         let flow_rate_pos = tokens[0].find("rate=").unwrap() + 5;
         let flow_rate = tokens[0][flow_rate_pos..].to_string();
         let to_valves_pos_w = s.find("valves ");
         let to_valves_pos;
         if to_valves_pos_w == None {
             to_valves_pos = s.find("valve ").unwrap() + 6;
-        }else {
+        } else {
             to_valves_pos = to_valves_pos_w.unwrap() + 7;
         }
         let to_valves = s[to_valves_pos..].to_string();
@@ -108,6 +108,43 @@ fn main() {
     }
     let st_valve = get_valve(&"AA".to_string(), &mut mp_valve, &mut _cnt);
     let vis = vec![false; n];
-    let ans = dfs(st_valve, 0, &vis, &dist, &fr);
-    println!("{}", ans);
+    let mut ma = dfs(st_valve, 0, &vis, &dist, &fr);
+    let mut idxs: Vec<usize> = Vec::new();
+    for i in 0..n {
+        if fr[i] > 0 {
+            idxs.push(i);
+        }
+    }
+    let m = idxs.len();
+    // println!("m {}", m);
+    for msk in 0..(1 << m) {
+        // if msk % 1000 == 0 {
+        //     println!("{}", msk);
+        // }
+        let mut vis1 = vec![false; n];
+        let mut vis2 = vec![false; n];
+        let mut cnt1 = 0;
+        let mut cnt2 = 0;
+        for i in 0..m {
+            if (msk & (1 << i)) > 0 {
+                let idx = idxs[i];
+                vis1[idx] = true;
+                cnt1 += 1;
+            } else {
+                let idx = idxs[i];
+                vis2[idx] = true;
+                cnt2 += 1;
+            }
+        }
+        // to save time
+        if cnt1 >= 2 * cnt2 || cnt2 >= 2 * cnt1 {
+            continue;
+        }
+        // let mut vis1 = vec![false; n];
+        let ans1 = dfs(st_valve, 4, &vis1, &dist, &fr);
+        let ans2 = dfs(st_valve, 4, &vis2, &dist, &fr);
+        // println!("{} {} {}", msk, ans1, ans2);
+        ma = std::cmp::max(ma, ans1 + ans2);
+    }
+    println!("{}", ma);
 }
